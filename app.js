fa07a77e2066127f912a21bff5c462f0bd2dda21
SP Singh <96386524+Codewithsps@users.noreply.github.com>
2024-05-17 18:55:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let month = "Rain";
+if (month === "jan") {
+    console.log("Winter is approaching");
+}
+else if (month === "Summer") {
+    console.log("Summer is approaching");
+}
+else if (month === "Rain") {
+    console.log("Rain is approaching");
+} else {
+    console.log("data wrong");
+}
+
+let fisrName = prompt("Enter First Name");
+let lastName = prompt("Enter Last Name");
+alert( `Nice to meet you  ${fisrName + lastName}`)
